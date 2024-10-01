9afc6ead509f7c6ece22f002e3f5a75e0ac32063
zahaa Muhanna <106498282+zahaaMuhanna@users.noreply.github.com>
2024-10-01 14:04:34 +0300

    Time: 106 ms (80.24%) | Memory: 54.5 MB (64.52%) - LeetSync

---
+++ 13-roman-to-integer/roman-to-integer.js
@@ -0,0 +1,28 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var romanToInt = function (s) {
+    const romanMap = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000,
+    }
+
+    let result=0;
+    for(let i=0;i<s.length;i++){
+        let current = romanMap[s[i]];
+        let next = romanMap[s[i+1]];
+
+        if(current < next){
+            result -= current;
+        }else{
+            result+=current;
+        }
+    }
+    return result;
+};
