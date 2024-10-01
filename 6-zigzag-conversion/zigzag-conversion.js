3bba689917c9ad70471ba13fac071b146986b8bd
zahaa Muhanna <106498282+zahaaMuhanna@users.noreply.github.com>
2024-10-01 15:47:03 +0300

    Time: 89 ms (61.28%) | Memory: 53.7 MB (83.42%) - LeetSync

--- 6-zigzag-conversion/zigzag-conversion.js
+++ 6-zigzag-conversion/zigzag-conversion.js
@@ -6,25 +6,24 @@
 var convert = function(s, numRows) {
     if(numRows===1 || numRows >= s.length){
         return s;
-    } 
+    } // check the number of rows
 
-    let indx=0;
-    let direction=1;
+    let indx=0; // current index
+    let direction=1; // the direction even 1 or -1 
 
-    const rows=Array.from({length:numRows}, ()=>'' );
+    const rows=Array.from({length:numRows}, ()=>'' ); // array of string
 
     for(const char of s){
-        rows[indx] += char;
-
+        rows[indx] += char;// push to rows array
         if(indx === 0){
-            direction = 1;
+            direction = 1; // check if the index in the top and the direction will be 1 (downwards)
         }
         else if(indx === numRows -1 ){
-            direction = -1;
+            direction = -1;  // check if the index in the bottom and the direction will be -1 (upwards)
         }
-        indx += direction;
+        indx += direction; // Update the row index based on direction
     }
 
-    return rows.join('');
+    return rows.join(''); // join the string array 
     
 };
