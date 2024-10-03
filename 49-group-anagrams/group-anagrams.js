d264ed69fbccbaf444d08be73aa264f062a6b166
zahaa Muhanna <106498282+zahaaMuhanna@users.noreply.github.com>
2024-10-03 15:20:16 +0300

    Time: 112 ms (67.89%) | Memory: 62.3 MB (79.96%) - LeetSync

--- 49-group-anagrams/group-anagrams.js
+++ 49-group-anagrams/group-anagrams.js
@@ -3,14 +3,14 @@
  * @return {string[][]}
  */
 var groupAnagrams = function(strs) {
-    const sortedStr = strs.map(word => word.split('').sort().join(''));
+    const sortedArray=strs.map(word=> word.split('').sort().join(''));
     const hash = {};
 
-    for(let i=0 ; i<strs.length ; i++){
-        if(!hash[sortedStr[i]]){
-            hash[sortedStr[i]] = [strs[i]];
-        }else {
-            hash[sortedStr[i]].push(strs[i]);
+    for(let i=0 ; i<strs.length ;i++){
+        if(!hash[sortedArray[i]]){
+            hash[sortedArray[i]] = [strs[i]];
+        }else{
+            hash[sortedArray[i]].push(strs[i]);
         }
     }
     return Object.values(hash);
