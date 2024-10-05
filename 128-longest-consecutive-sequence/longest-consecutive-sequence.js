6ab12fe069c9d9f07a0ccb5ec57102e046ca7792
zahaa Muhanna <106498282+zahaaMuhanna@users.noreply.github.com>
2024-10-05 11:56:00 +0300

    Time: 103 ms (74.63%) | Memory: 73.3 MB (38.32%) - LeetSync

---
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var longestConsecutive = function(nums) {
+    const numSet = new Set(nums);
+    let longestStreak=0;
+    for(const num of numSet){
+        if(!numSet.has(num-1)){
+            let currentNum = num;
+            let currentStreak = 1;
+        
+        while(numSet.has(currentNum+1)){
+            currentNum += 1;
+            currentStreak += 1;
+        }
+            longestStreak = Math.max(longestStreak,currentStreak);
+        }
+    }
+
+    return longestStreak;
+};
