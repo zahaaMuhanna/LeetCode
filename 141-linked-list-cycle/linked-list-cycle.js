1b5a05699e497ac525924475c5c1598d8a257a8c
zahaa Muhanna <106498282+zahaaMuhanna@users.noreply.github.com>
2024-10-14 20:21:47 +0300

    Time: 63 ms (63.23%) | Memory: 53 MB (54.63%) - LeetSync

--- 141-linked-list-cycle/linked-list-cycle.js
+++ 141-linked-list-cycle/linked-list-cycle.js
@@ -17,9 +17,7 @@ var hasCycle = function(head) {
     while(fast && fast.next){
         slow = slow.next;
         fast = fast.next.next;
-        if(slow === fast){
-            return true;
-        }
+        if(slow === fast) return true;
     }
     return false;
 };
