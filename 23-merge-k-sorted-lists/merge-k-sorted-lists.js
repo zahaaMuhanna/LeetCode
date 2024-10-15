06820c8c72d2d6caaec5de320adc2591cca1a76a
zahaa Muhanna <106498282+zahaaMuhanna@users.noreply.github.com>
2024-10-15 14:48:12 +0300

    Time: 75 ms (91.03%) | Memory: 57.9 MB (24.87%) - LeetSync

---
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.js
@@ -0,0 +1,37 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode[]} lists
+ * @return {ListNode}
+ */
+var mergeKLists = function(lists) {
+    let minHeap = [];
+    for(let list of lists){
+        if(list!==null){
+        minHeap.push(list);
+        }
+    }
+    minHeap.sort((a,b)=>a.val - b.val);
+
+    const dummy = new ListNode();
+    let current = dummy;
+    while(minHeap.length > 0){
+        let smallestNode=minHeap.shift();
+
+        current.next = smallestNode;
+        current = current.next;
+
+        if(smallestNode.next !== null ){
+            minHeap.push(smallestNode.next);
+            minHeap.sort((a,b)=> a.val-b.val );
+
+        }
+    }
+
+    return dummy.next;
+};
